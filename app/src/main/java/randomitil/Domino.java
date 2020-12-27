3984c530590c2a1b8458654013899ba812fe32ca
catmanisacatlord <catmanisacatlord@gmail.com>
2020-12-27 15:22:10 -0700

    quick domino fix

--- app/src/main/java/randomitil/Domino.java
+++ app/src/main/java/randomitil/Domino.java
@@ -10,9 +10,26 @@ public class Domino
     {
         this(Direction.UP,false);
     }
+
     public Domino (Direction direction, boolean moved)
     {
         this.direction = direction;
+        this.moved = moved;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    public boolean isMoved() {
+        return moved;
+    }
+
+    public void setMoved(boolean moved) {
         this.moved = moved;
     }
 }
