3056fa5ff1594b8f1ddb5df2adfc222f57b81665
catmanisacatlord <catmanisacatlord@gmail.com>
2020-12-29 15:20:33 -0700

    set up skeleton

---
+++ app/src/main/java/randomitil/DominoIteration.java
@@ -0,0 +1,36 @@
+package randomitil;
+
+public class DominoIteration {
+    
+    private boolean[][] emptyTiles;
+    private boolean[][] emptySquares;
+    double tilingBias;
+
+    private AztecDiamond aztecDiamond;
+
+    public DominoIteration (AztecDiamond aztecDiamond) {
+        this.aztecDiamond = aztecDiamond;
+        findEmptyTiles();  
+    }
+
+    private void findEmptyTiles() {
+        emptyTiles = new boolean[this.aztecDiamond.getSize()][this.aztecDiamond.getSize()];
+        for (int i = 0; i < this.emptyTiles.length; i++) {
+            for (int j = 0; j < this.emptyTiles[i].length; j++) {
+                if(this.aztecDiamond.getTile(i, j) == null) {
+                    if(this.aztecDiamond.hasNoNeighbors(i, j)) {
+                        emptyTiles[i][j] = true;
+                    }
+                }
+            }    
+        }
+    }
+
+    public void moveDominos(int expansionSize) {
+        
+    }
+
+    public AztecDiamond getAztecDiamond() {
+        return aztecDiamond;
+    }
+}
