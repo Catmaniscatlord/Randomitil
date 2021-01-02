fa719cc4cde279ba17fe323f66d1bc5a3bb7ae40
catmanisacatlord <catmanisacatlord@gmail.com>
2021-01-02 16:51:42 -0700

    more output for better debugging

--- app/src/main/java/randomitil/App.java
+++ app/src/main/java/randomitil/App.java
@@ -1,34 +1,39 @@
 package randomitil;
 
+import java.util.Scanner;
+
 import randomitil.animation.*;
 
 public class App {
     public static void main(String[] args) {
         // Set up UI
-        DominoDrawer ui_drawer = new DominoDrawer();
-        DominoInterface.setup(ui_drawer);
-
+        
         // Set up Diamond for rendering
-        AztecDiamond diamond = new AztecDiamond(2,3);
+        AztecDiamond diamond = new AztecDiamond(2, 2);
         
-        System.out.println(diamond);
         DominoIteration kevin = new DominoIteration(diamond);
-        kevin.fillEmptySquares();
-        System.out.println(diamond);
-        kevin.fillEmptyTiles();
-        System.out.println(diamond);
-        kevin.moveDominos(1);
-        diamond = kevin.getAztecDiamond();
-        System.out.println(diamond);
-        if(diamond.checkTiles().isEmpty()) {
-            System.out.println("we gucci");
-        }
-        else {
-            System.out.println("oh booty cheeks");
-            System.out.println(diamond.checkTiles());
-        }
+        kevin.setanimationMode(true);
         
-        // Update Diamond
-        ui_drawer.updateDiamond(diamond, kevin, diamond.getSize());
+        for (int i = 0; i < 10; i++) {
+            DominoDrawer ui_drawer = new DominoDrawer();
+            DominoInterface.setup(ui_drawer);
+            kevin.fillEmptySquares();
+            //kevin.fillEmptyTiles();
+            System.out.println("generated tiles");
+            System.out.println(kevin.getGeneratedTiles());
+            System.out.println("new tiles");
+            System.out.println(kevin.getAztecDiamond());
+            ui_drawer.updateDiamond(kevin.getAztecDiamond(), kevin, kevin.getAztecDiamond().getSize());
+            if (kevin.getAztecDiamond().checkTiles().isEmpty()) {
+                System.out.println("we gucci");
+            } else {
+                System.out.println("oh booty cheeks");
+            }
+            kevin.moveDominos(1);
+            System.out.println("Removed tiles");
+            System.out.println(kevin.getRemovedTiles());
+            System.out.println("moved tiles");
+            System.out.println(kevin.getAztecDiamond());
+       }
     }
 }
