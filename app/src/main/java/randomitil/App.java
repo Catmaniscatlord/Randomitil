1792885931fdc3587f8df5a8ec8b9c921e12c421
catmanisacatlord <catmanisacatlord@gmail.com>
2021-01-02 19:36:52 -0700

    fixed the main file so they now interact properly

--- app/src/main/java/randomitil/App.java
+++ app/src/main/java/randomitil/App.java
@@ -7,6 +7,8 @@ import randomitil.animation.*;
 public class App {
     public static void main(String[] args) {
         // Set up UI
+        DominoDrawer ui_drawer = new DominoDrawer();
+        DominoInterface.setup(ui_drawer);
         
         // Set up Diamond for rendering
         AztecDiamond diamond = new AztecDiamond(2, 2);
@@ -14,26 +16,21 @@ public class App {
         DominoIteration kevin = new DominoIteration(diamond);
         kevin.setanimationMode(true);
         
+        kevin.fillEmptySquares();
+        System.out.println("new tiles");
+        System.out.println(kevin.getAztecDiamond());
+    
         for (int i = 0; i < 10; i++) {
-            DominoDrawer ui_drawer = new DominoDrawer();
-            DominoInterface.setup(ui_drawer);
+            kevin.moveDominos(1);
             kevin.fillEmptySquares();
-            //kevin.fillEmptyTiles();
-            System.out.println("generated tiles");
-            System.out.println(kevin.getGeneratedTiles());
+            kevin.fillEmptyTiles();
             System.out.println("new tiles");
             System.out.println(kevin.getAztecDiamond());
-            ui_drawer.updateDiamond(kevin.getAztecDiamond(), kevin, kevin.getAztecDiamond().getSize());
-            if (kevin.getAztecDiamond().checkTiles().isEmpty()) {
-                System.out.println("we gucci");
-            } else {
+            if (!kevin.getAztecDiamond().checkTiles().isEmpty()) {
                 System.out.println("oh booty cheeks");
             }
-            kevin.moveDominos(1);
-            System.out.println("Removed tiles");
-            System.out.println(kevin.getRemovedTiles());
-            System.out.println("moved tiles");
-            System.out.println(kevin.getAztecDiamond());
-       }
+        }
+
+        ui_drawer.updateDiamond(kevin.getAztecDiamond(), kevin, kevin.getAztecDiamond().getSize());
     }
 }
